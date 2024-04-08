64f193b839a861e8845b26b66ec2d46c2090acdb
Simony Cogoy <75379848+Sainomy@users.noreply.github.com>
2024-04-08 16:27:25 -0300

    Update memoria.c

--- memoria.c
+++ memoria.c
@@ -4,19 +4,18 @@
 
 typedef struct {
     char instrucao[17];
+    char opcode[7];
 } Memoria;
 
 void carregarMemoria(char *nomeArquivo, Memoria *mem, int *count);
+void decodificarOpCode(Memoria *mem, int count);
 
 int main() {
     Memoria mem[256];
     int count = 0;
 
     carregarMemoria("instrucoes.txt", mem, &count);
-
-    for (int i = 0; i < count; i++) {
-        printf("%s\n", mem[i].instrucao);
-    }
+    decodificarOpCode(mem, count);
 
     return 0;
 }
@@ -29,10 +28,25 @@ void carregarMemoria(char *nomeArquivo, Memoria *mem, int *count) {
         printf("Erro ao abrir o arquivo.\n");
         return;
     }
-
+// esse 16s pega 16 caracteres char
     while (fscanf(arquivo, "%16s", mem[*count].instrucao) != EOF && *count < 100) {
+
+        strncpy(mem[*count].opcode, mem[*count].instrucao, 6);
+        mem[*count].opcode[6] = '\0';
         (*count)++;
     }
 
     fclose(arquivo);
+
+    for (int i = 0; i < *count; i++) {
+       printf("%s\n", mem[i].instrucao);
+   }
+}
+
+void decodificarOpCode(Memoria *mem, int count) {
+    printf("Decodificação do OpCode:\n");
+
+    for (int i = 0; i < count; i++) {
+        printf("Instrução %d: %s OpCode = %s\n", i + 1, mem[i].instrucao, mem[i].opcode);
+    }
 }
