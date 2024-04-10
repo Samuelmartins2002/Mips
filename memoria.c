da26ddf550afa173d0f626291029ee0d4dfc02ce
OtavioFarias <137441117+OtavioFarias@users.noreply.github.com>
2024-04-09 22:29:01 -0300

    correção de bugs, ULA, função decimal para binário

--- memoria.c
+++ memoria.c
@@ -6,18 +6,24 @@
 typedef struct {
     char instrucao[17];
     char opcode[5];
+    char controle[4];
+    char value[9];
+    char value1[9];
+    char resul[9];
 } Memoria;
 
-int bi_dec(char mem[17]);
+void dec_bi(Memoria *mem, int k, int *count);
+void ula(Memoria *mem, int *count);
+int bi_dec(char *mem);
 void carregarMemoria(char *nomeArquivo, Memoria *mem, int *count);
-void decodificarOpCode(Memoria *mem, int count);
+void decodificarOpcode(Memoria *mem, int *count);
 
 int main() {
     Memoria mem[256];
     int count = 0;
 
     carregarMemoria("instrucoes.txt", mem, &count);
-    decodificarOpcode(mem, count);
+    decodificarOpcode(mem, &count);
     return 0;
 }
 
@@ -43,13 +49,11 @@ void carregarMemoria(char *nomeArquivo, Memoria *mem, int *count) {
    }
 }
 
-int bi_dec(char mem[17]){
-	char vet[17];
-	strcpy(vet,mem);
-	int i=strlen(vet);
+int bi_dec(char *mem){
+	int i=strlen(mem);
 	int value,deci=0,k=0;
 	for(int j=i;j>0;j--){
-		if(vet[j-1]=='0'){
+		if(mem[j-1]=='0'){
 			value=0;
 		}
 		else{
@@ -61,9 +65,26 @@ int bi_dec(char mem[17]){
 	return(deci);
 }
 
-void decodificarOpcode(Memoria *mem, int count) {
+void deci_bi(Memoria *mem, int k, int *count){
+	int t=0;
+	for(int i=0;i<8;i++){
+		mem[*count].resul[i]='0';
+	}
+	while(k!=0){
+		if(k%2==1){
+			mem[*count].resul[8-t]='1';
+		}
+		else{
+			mem[*count].resul[8-t]='1';	
+		}
+		t++;
+		k=k/2;
+	}
+}
+
+void decodificarOpcode(Memoria *mem, int *count) {
 	int n;
-	n=bi_dec(mem[count].opcode);
+	n=bi_dec(mem[*count].opcode);
 	switch(n){
 		case 0:
 			printf("Tipo R\n");
@@ -87,4 +108,39 @@ void decodificarOpcode(Memoria *mem, int count) {
 			printf("Instrução inválida\n");
 			break;
 	}
+}
+
+void ula(Memoria *mem, int *count){
+	enum comando{add=0,sub=2,or=4,and=5};
+	enum comando n=bi_dec(mem[*count].controle);
+        int x=bi_dec(mem[*count].value);
+	int y=bi_dec(mem[*count].value);
+	switch(n){
+		case add:
+			 deci_bi(mem,x+y,count);
+			break;
+		case sub:
+			deci_bi(mem,x-y,count);
+			break;
+		case and:
+        		for(int j=0;j<8;j++){
+                		if(mem[*count].value[j]==mem[*count].value1[j] && mem[*count].value[j]=='1'){
+					mem[*count].resul[j]='1';
+                        	}
+				else{
+					mem[*count].resul[j]='0';
+				}
+			}
+			break;
+                case or:
+			for(int j=0;j<8;j++){
+                                if(mem[*count].value1[j]=='1' || mem[*count].value[j]=='1'){
+                                        mem[*count].resul[j]='1';
+                                }
+                                else{
+                                        mem[*count].resul[j]='0';
+                                }
+                        }
+                        break;
+		}
 }
