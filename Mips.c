9d3a36acbe668e454f78baaa042acc22085697f6
OtavioFarias <137441117+OtavioFarias@users.noreply.github.com>
2024-04-11 13:46:42 -0300

    Tentativa de fazer add e addi, problema nos registradores

--- Mips.c
+++ Mips.c
@@ -3,12 +3,36 @@
 #include <stdlib.h>
 #include "mips.h"
 
-
-int main() {
+int main(){
     Memoria mem[256];
-    int *count=malloc(sizeof(int));
+    int *count = malloc(sizeof(int));
+    char **registrador;
+
+    if (count == NULL) {
+      printf("Memory allocation failed for count\n");
+      return 1; // Return an error status
+    }
+
+    registrador = malloc(8 * sizeof(char *)); 
+
+    if (registrador == NULL) {
+      printf("Memory allocation failed for registrador\n");
+      free(count); // Free the previously allocated memory
+      return 1; // Return an error status
+    }
+
     *count = 0;
+
     carregarMemoria("instrucoes.txt", mem, count);
     decodificarOpcode(mem, count);
+    pc(mem, count, registrador);
+
+    // Free allocated memory before returning
+    free(count);
+    for (int i = 0; i < 8; i++) {
+      free(registrador[i]);
+    }
+    free(registrador);
+
     return 0;
-}
+  }
--- mips.h
+++ mips.h
@@ -9,10 +9,6 @@
 typedef struct {
     char instrucao[17];
     char opcode[5];
-    char controle[4];
-    char value[9];
-    char value1[9];
-    char resul[9];
     char funct[4];
     char rs[4];
     char rt[4];
@@ -21,11 +17,13 @@ typedef struct {
     char imm[7];
 } Memoria;
 
+void UC(Memoria *mem, int *count, char **registrador);
+void pc(Memoria *mem, int *count, char **registrador);
 void tipo_R(Memoria *mem, int *count);
 void tipo_I(Memoria *mem, int *count);
 void tipo_J(Memoria *mem, int *count);
-void dec_bi(Memoria *mem, int k, int *count);
-void ula(Memoria *mem, int *count);
+void dec_bi(char *output, int k, int *count);
+void ula(char *funct, char *valor,char *valor1, char *output, int *count);
 int bi_dec(char *mem);
 void carregarMemoria(char *nomeArquivo, Memoria *mem, int *count);
 void decodificarOpcode(Memoria *mem, int *count);
@@ -54,6 +52,7 @@ void carregarMemoria(char *nomeArquivo, Memoria *mem, int *count) {
 }
 
 int bi_dec(char *mem){
+  printf("oi");
 	int i=strlen(mem);
 	int value,deci=0,k=0;
 	for(int j=i;j>0;j--){
@@ -69,17 +68,17 @@ int bi_dec(char *mem){
 	return(deci);
 }
 
-void dec_bi(Memoria *mem, int k, int *count){
+void dec_bi(char *output, int k, int *count){
 	int t=0;
 	for(int i=0;i<8;i++){
-		mem[*count].resul[i]='0';
+		output[i]='0';
 	}
 	while(k!=0){
 		if(k%2==1){
-			mem[*count].resul[8-t]='1';
+			output[8-t]='1';
 		}
 		else{
-			mem[*count].resul[8-t]='1';	
+			output[8-t]='1';	
 		}
 		t++;
 		k=k/2;
@@ -120,39 +119,40 @@ void decodificarOpcode(Memoria *mem, int *count) {
 	}
 }
 
-void ula(Memoria *mem, int *count){
+void ula(char *funct, char *valor, char *valor1, char *output, int *count){
 	enum comando{add=0,sub=2,or=4,and=5};
-	enum comando n=bi_dec(mem[*count].controle);
-        int x=bi_dec(mem[*count].value);
-	int y=bi_dec(mem[*count].value1);
+	enum comando n=bi_dec(funct);
+        int x=bi_dec(valor);
+	int y=bi_dec(valor1);
 	switch(n){
 		case add:
-			 dec_bi(mem,x+y,count);
+			 dec_bi(output,x+y,count);
 			break;
 		case sub:
-			dec_bi(mem,x-y,count);
+			dec_bi(output,x-y,count);
 			break;
 		case and:
-        		for(int j=0;j<8;j++){
-                		if(mem[*count].value[j]==mem[*count].value1[j] && mem[*count].value[j]=='1'){
-					mem[*count].resul[j]='1';
-                        	}
-				else{
-					mem[*count].resul[j]='0';
-				}
-			}
-			break;
-                case or:
-			for(int j=0;j<8;j++){
-                                if(mem[*count].value1[j]=='1' || mem[*count].value[j]=='1'){
-                                        mem[*count].resul[j]='1';
-                               }
-                                else{
-                                        mem[*count].resul[j]='0';
-                                }
-                        }
-                        break;
-		}
+      for(int j=0;j<8;j++){
+        if(valor[j]==valor1[j] && valor[j]=='1'){
+          
+          output[j]='1';
+          }
+        else{
+          output[j]='0';
+        }
+      }
+      break;
+      case or:
+for(int j=0;j<8;j++){
+  if(valor[j]=='1' || valor1[j]=='1'){
+          output[j]='1';
+ }
+  else{
+          output[j]='0';
+  }
+}
+break;
+}
 }
 
 void tipo_R(Memoria *mem, int *count){
@@ -180,6 +180,80 @@ void tipo_J(Memoria *mem, int *count){
         }
 }
 
+void iniciarRegistrador(char **registrador){
+  for(int i=0;i<8;i++){
+    registrador[i]=*(char **)malloc(17*sizeof(char));
+  }
+}
+
+void DadosRegistrador(char **registradores, char *dados, char *end, int chose, char *des){
+  printf("teste");
+  printf("%c",end[0]);
+  int k=bi_dec(end);
+  printf("%i",k);
+  switch(chose){
+    case 0:
+      strcpy(registradores[k],dados);
+      break;
+    case 1:
+      strcpy(des,registradores[k]);
+      break;
+  }
+}
+
+void pc(Memoria *mem, int *count,char **registrador){
+  decodificarOpcode(mem, count);
+  UC(mem, count, registrador);
+  printf("sei la 2\n");
+  if(mem[*count].instrucao[0] == '\0'){
+    return;
+  }
+  else{
+    UC(mem, count,registrador);
+    pc(mem,count+1,registrador);
+  }
+}
+
+void UC(Memoria *mem, int *count, char **registrador){
+ printf("inicio do sonho\n");
+ int k=bi_dec(mem[*count].opcode);
+ char *value;
+ value=(char*)malloc(17*sizeof(char));
+ char *value1; 
+ value1=(char*)malloc(17*sizeof(char));
+ char *output;
+ output=(char*)malloc(17*sizeof(char));
+ char *null1;
+ null1=(char*)malloc(17*sizeof(char));
+for(int i=0;i<16;i++){
+  null1[i]='0';
+  value[i]='0';
+  value1[i]='0';
+}
+  printf("até aqui tudo bem\n");
+  switch(k){
+   case 0:
+      DadosRegistrador(registrador, null1, mem[*count].rs, 1, value);
+      DadosRegistrador(registrador, null1, mem[*count].rt, 1, value1);
+      ula(mem[*count].funct, value,value1,output,count);
+      DadosRegistrador(registrador, output, mem[*count].rd, 0, null1);
+      break;
+  case 4:
+      printf("aqui é a cagada\n");
+      DadosRegistrador(registrador, null1, mem[*count].rs, 1, value); //achou
+      printf("ta quente\n");
+      ula(null1, value, mem[*count].imm, output, count);
+      DadosRegistrador(registrador, output, mem[*count].rd, 0, null1);
+ }
+}
+
+void inciarRegistrador(char **registrador){
+  registrador=(char**)malloc(8*sizeof(char*));
+  for(int i=0;i<16;i++){
+    registrador[i]=(char*)malloc(17*sizeof(char));
+    registrador[i]='0';
+  }
+}
 #endif
 
 
